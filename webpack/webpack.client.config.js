8f226ff667f223307753c87ef95e395f9232cbae
mx <maoxuheng@primeledger.cn>
2018-08-07 16:42:00 +0800

    增加开发client的sourcemap

--- webpack/webpack.client.config.js
+++ webpack/webpack.client.config.js
@@ -4,9 +4,11 @@ const config = require('./webpack.base.config');
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin');
 const path = require('path');
 const CleanWepackPlugin = require('clean-webpack-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = merge(config, {
   entry: path.join(__dirname, '../client/entry-client.js'),
+  devtool: isProduction ? undefined : 'cheap-module-eval-source-map',
   plugins: [
     new CleanWepackPlugin('app/public', { root: path.join(__dirname, '..') }),
     new webpack.optimize.CommonsChunkPlugin({
